82c28da477828fb130c18e65ef33045451749183
yuliakruhlyk <100228348+YuliaKruglyk@users.noreply.github.com>
2022-10-03 15:12:08 +0200

    Update rotate.c

--- rotate.c
+++ rotate.c
@@ -12,29 +12,29 @@
 
 #include "push_swap.h"
 
-void	ra(t_stack *stack, int check)
+void	ra(t_stack *stack, int print)
 {
 	if (stack->len_a > 1)
 		stack->stack_a = stack->stack_a->next;
-	if (check)
+	if (print)
 		write(1, "ra\n", 3);
 }
 
-void	rb(t_stack *stack, int check)
+void	rb(t_stack *stack, int print)
 {
 	if (stack->len_b > 1)
 		stack->stack_b = stack->stack_b->next;
-	if (check)
+	if (print)
 		write(1, "rb\n", 3);
 }
 
-void	rr(t_stack *stack, int check)
+void	rr(t_stack *stack, int print)
 {
 	if (stack->len_a > 1 && stack->len_b > 1)
 	{
 		ra(stack, 0);
 		rb(stack, 0);
-		if(check)
+		if (print)
 			write(1, "rr\n", 3);
 	}
 }
