b846bfc6f5ad67b2ac7fb96245ac8e9facef2ed6
yuliakruhlyk <100228348+YuliaKruglyk@users.noreply.github.com>
2022-10-03 15:11:31 +0200

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -12,29 +12,29 @@
 
 #include "push_swap.h"
 
-void	rra(t_stack *stack, int check)
+void	rra(t_stack *stack, int print)
 {
 	if (stack->len_a > 1)
 		stack->stack_a = stack->stack_a->prev;
-	if (check)
+	if (print)
 		write(1, "rra\n", 4);
 }
 
-void	rrb(t_stack *stack, int check)
+void	rrb(t_stack *stack, int print)
 {
 	if (stack->len_b > 1)
 		stack->stack_b = stack->stack_b->prev;
-	if (check)
+	if (print)
 		write(1, "rrb\n", 4);
 }
 
-void	rrr(t_stack *stack, int check)
+void	rrr(t_stack *stack, int print)
 {
 	if (stack->len_a > 1 && stack->len_b > 1)
 	{
 		rra(stack, 0);
 		rrb(stack, 0);
-		if (check)
+		if (print)
 			write(1, "rrr\n", 4);
 	}
 }
