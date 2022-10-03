a0137909210b096157ed548d7a39081e378dd624
yuliakruhlyk <100228348+YuliaKruglyk@users.noreply.github.com>
2022-10-03 15:00:08 +0200

    Update check_num.c

--- check_num.c
+++ check_num.c
@@ -15,19 +15,16 @@
 void	error(void)
 {
 	ft_putstr_fd("Error\n", 2);
-	exit(0);
+	exit(EXIT_SUCCESS);
 }
 
 void	free_ps(char **arr, int print)
 {
 	int	i;
 
-	i = 0;
-	while (arr[i])
-	{
+	i = -1;
+	while (arr[++i])
 		free(arr[i]);
-		i++;
-	}
 	free(arr);
 	if (print)
 		error();
@@ -35,34 +32,56 @@ void	free_ps(char **arr, int print)
 
 void	check_num(char **arr)
 {
-	int	i;
-	int	j;
+	int			i;
+	int			j;
 	long long	num;
 
-	i = 0;
-	while (arr[i])
+	i = -1;
+	while (arr[++i])
 	{
 		j = 0;
-		while(arr[i][j])
+		while (arr[i][j])
 		{
-			if(arr[i][j] == '-')
+			if (arr[i][j] == '-')
 				j++;
 			if (!(ft_isdigit(arr[i][j])))
 				free_ps(arr, 1);
 			j++;
 		}
 		num = ft_atoi(arr[i]);
-		if (num < MIN_INT || num > MAX_INT)
+		if (num > MAX_INT || num < MIN_INT)
 			free_ps(arr, 1);
-		i++;
 	}
+}
+
+char	**get_line(char **av)
+{
+	int		i;
+	char	*line;
+	char	*tmp;
+	char	**arr;
+
+	line = ft_strdup(av[1]);
+	i = 1;
+	while (av[++i])
+	{
+		tmp = line;
+		line = ft_strjoin(line, " ");
+		free(tmp);
+		tmp = line;
+		line = ft_strjoin(line, av[i]);
+		free(tmp);
+	}
+	arr = ft_split(line, ' ');
+	free(line);
+	return (arr);
 }
 
 void	check_doubles(char **arr)
 {
-	int	i;
-	int	j;
-	
+	int		i;
+	int		j;
+
 	i = 0;
 	while (arr[i])
 	{
@@ -75,33 +94,4 @@ void	check_doubles(char **arr)
 		}
 		i++;
 	}
-}
-
-char	**get_line(char **av)
-{
-	int		i;
-	char	*line;
-	char	*tmp;
-	char	**arr;
-
-	line = ft_strdup(av[1]);
-	i = 2;
-	while(av[i])
-	{
-		tmp = line;
-		line = ft_strjoin(line, " ");
-		free(tmp);
-		tmp = line;
-		line = ft_strjoin(line, av[i]);
-		free(tmp);
-		i++;
-	}
-	arr = ft_split(line, ' ');
-	free(line);
-	// while (*arr)
-	// {
-	// 	printf("%s\n", *arr);
-	// 	arr++;
-	// }
-	return(arr);
 }
